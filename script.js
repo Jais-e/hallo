7cf09780bec2c1a97b52b0aedac9f166e9d04500
Jais Kurecko Eriksen <eaajaeri6@students.eaaa.dk>
2018-10-05 17:36:38 +0200

    Lidt fremskridt...

--- script.js
+++ script.js
@@ -1,38 +1,55 @@
+// Skifter overskrift til valgt kategori
+
+$( "#cat1" )
+  .change(function() {
+    var str = "";
+    $( "#cat1 option:selected" ).each(function() {
+      str += $( this ).text() + " ";
+    });
+    $( "#title1" ).text( str + ' is selected') ;
+  });
+
+
+  $( "#cat2" )
+    .change(function() {
+      var str = "";
+      $( "#cat2 option:selected" ).each(function() {
+        str += $( this ).text() + " ";
+      });
+      $( "#title2" ).text( str + ' is selected') ;
+    });
+
+    $( "#cat3" )
+      .change(function() {
+        var str = "";
+        $( "#cat3 option:selected" ).each(function() {
+          str += $( this ).text() + " ";
+        });
+        $( "#title3" ).text( str + ' is selected') ;
+      });
+
 // Dette er vores randomizer - P.t. kan den vælge tre tilfældige items ud og sætte dem ind i html'en. Vi skal udvide den lidt :-)
+$('button').click(function(){
 
 
-$('#randomize').click(function() {
-
-  // Vi skal lave 4 var's - Een for hver kategori - "names" var er bare et eksempel jeg brugte da jeg skulle have den til at virke.
-  // Denne type "array" kan indeholde flere værdier pr item - VI skal lave 3: Et billede, et stednavn og en beskrivelse
-
-  if ( $('#cat1').val() == 1) {
-
-
-  var bars = [
-    ["Kai", " Andersen"],
-    ["Anders", " Lassen"],
-    ["Petter", " Brandt"],
-    ["Anne", " Madsen"]
-  ];
-
-  var randName1 = [];
-
-
+  var names = [
+    ["Billede1", " Titel1", " Beskrivelse1"],
+    ["Billede2", " Titel2", " Beskrivelse2"],
+    ["Billede3", " Titel3", " Beskrivelse3"],
+    ["Billede4", " Titel4", " Beskrivelse4"]
+    ];
+  var randName = [];
+  do {
+    randName[randName.length] = names.splice(
+                                  Math.floor(Math.random() * names.length)
+                                , 1)[0];
+  } while (randName.length < 3);
+$('#1').html(randName[0]);
+$('#1').html(randName[1]);
+$('#1').html(randName[2]);
+  console.log(randName);
 
 
-    do {
-      randName1[randName1.length] = bars.splice(
-        Math.floor(Math.random() * bars.length), 1)[0];
-    } while (randName1.length < 3);
-
-    //Indsætter de 3 items i 3 forskellige div's
-    $('#dest1').html(randName1[0]);
-    $('#dest2').html(randName1[1]);
-    $('#dest3').html(randName1[2]);
-    console.log(randName);
-
-    }
 });
 
 
