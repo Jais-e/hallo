574bb43eb03d0ddb151b8cd7cb187d7accae47fc
Jais Kurecko Eriksen <eaajaeri6@students.eaaa.dk>
2018-10-05 08:30:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 $('button').click(function(){
 
 // Vi skal lave 4 var's - Een for hver kategori - "names" var er bare et eksempel jeg brugte da jeg skulle have den til at virke.
+// Denne type "array" kan indeholde flere værdier pr item - VI skal lave 3: Et billede, et stednavn og en beskrivelse
   var names = [
     ["Kai", " Andersen"],
     ["Anders", " Lassen"],
@@ -16,6 +17,8 @@ $('button').click(function(){
                                   Math.floor(Math.random() * names.length)
                                 , 1)[0];
   } while (randName.length < 3);
+
+  //Indsætter de 3 items i 3 forskellige div's
 $('#1').html(randName[0]);
 $('#2').html(randName[1]);
 $('#3').html(randName[2]);
