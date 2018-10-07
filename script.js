323e35e0df0c453c33dc02c5b4b1fbcec84b0fe5
Jais Kurecko Eriksen <eaajaeri6@students.eaaa.dk>
2018-10-07 19:05:24 +0200

    Random fixed
    Nu virker det med randomize i forhold til valg af kategori - I 1. slot... Skal lige hitte ud af hvodden dælen vi får den til at fatte at der er 3 slots.
    Har også fået det til at virke med et billede i resultatet - lige nu bare Hallo logo, men vi skal lige finde et billede af hvert sted vi kan bruge

--- script.js
+++ script.js
@@ -7,13 +7,9 @@ $( "#cat1" )
       str += $( this ).text() + " ";
     });
     $( "#title1" ).text( str + ' is selected') ;
-
+    selectedCat =$( "#cat1 option:selected" ).val();
     console.log(str);
-
-if (str == 'bar'){
-
-}
-
+    console.log(selectedCat);
   });
 
 
@@ -24,40 +20,123 @@ if (str == 'bar'){
         str += $( this ).text() + " ";
       });
       $( "#title2" ).text( str + ' is selected') ;
+      selectedCat = str;
+      console.log(str);
+      console.log(selectedCat);
     });
 
-    $( "#cat3" )
+  $( "#cat3" )
       .change(function() {
         var str = "";
         $( "#cat3 option:selected" ).each(function() {
           str += $( this ).text() + " ";
         });
         $( "#title3" ).text( str + ' is selected') ;
+        selectedCat = str;
+        console.log(str);
+        console.log(selectedCat);
       });
 
 
 
+
 // Dette er vores randomizer - P.t. kan den vælge tre tilfældige items ud og sætte dem ind i html'en. Vi skal udvide den lidt :-)
-$('button').click(function(){
+$('#randomize').click(function(){
+
+  var food =
+  [
+  ["<img src='img/logo1.png'/>", "<br/>Ali babas<br/>", "Beskrivelse1"],
+  ["<img src='img/logo1.png'/>", "<br/>Kowloon<br/>", "Beskrivelse2"],
+  ["<img src='img/logo1.png'/>", "<br/>Pita corner<br/>", "Beskrivelse3"],
+  ["<img src='img/logo1.png'/>", "<br/>A+ Siam Sushi<br/>", "Beskrivelse4"]
+  ];
+  var bars =
+    [
+  ["<img src='img/logo1.png'/>", "<br/>Fatter Eskil<br/>", "Beskrivelse1"],
+  ["<img src='img/logo1.png'/>", "<br/>G-Bar<br/>", "Beskrivelse2"],
+  ["<img src='img/logo1.png'/>", "<br/>Old Irish<br/>", "Beskrivelse3"],
+  ["<img src='img/logo1.png'/>", "<br/>Bodegaen<br/>", "Beskrivelse4"]
+  ];
+  var cult =
+    [
+  ["<img src='img/logo1.png'/>", "<br/>Steno Museet<br/>", "Beskrivelse1"],
+  ["<img src='img/logo1.png'/>", "<br/>Kunsthal Aarhus<br/>", "Beskrivelse2"],
+  ["<img src='img/logo1.png'/>", "<br/>Kvindemuseet<br/>", "Beskrivelse3"],
+  ["<img src='img/logo1.png'/>", "<br/>Marseillesborg slot<br/>", "Beskrivelse4"]
+  ];
+  var acvtivity =
+    [
+  ["<img src='img/logo1.png'/>", "<br/>Ø-Haven<br/>", "Beskrivelse1"],
+  ["<img src='img/logo1.png'/>", "<br/>Eventhall<br/>", "Beskrivelse2"],
+  ["<img src='img/logo1.png'/>", "<br/>Dyrehaven<br/>", "Beskrivelse3"],
+  ["<img src='img/logo1.png'/>", "<br/>Århus Boulders<br/>", "Beskrivelse4"]
+  ];
 
 
-  var names = [
-    ["Billede1 <br/>", "Titel1 <br/>", "Beskrivelse1"],
-    ["Billede2 <br/>", "Titel2 <br/>", "Beskrivelse2"],
-    ["Billede3 <br/>", "Titel3 <br/>", "Beskrivelse3"],
-    ["Billede4 <br/>", "Titel4 <br/>", "Beskrivelse4"]
-    ];
+
+
+
+
+
+
+// Hvis bar kategori er valgt
+if (selectedCat == 'bar-cat'){
+
   var randName = [];
   do {
-    randName[randName.length] = names.splice(
-                                  Math.floor(Math.random() * names.length)
+    randName[randName.length] = bars.splice(
+                                  Math.floor(Math.random() * bars.length)
                                 , 1)[0];
   } while (randName.length < 3);
 $('#one').html(randName[0]);
-
+$('select').hide();
+$('.selected').hide();
   console.log(randName);
+}
 
+// Hvis food kategori er valgt
+if (selectedCat == 'food-cat'){
 
+  var randName = [];
+  do {
+    randName[randName.length] = food.splice(
+                                  Math.floor(Math.random() * food.length)
+                                , 1)[0];
+  } while (randName.length < 3);
+$('#one').html(randName[0]);
+$('select').hide();
+$('.selected').hide();
+  console.log(randName);
+}
+
+// Hvis culture kategori er valgt
+if (selectedCat == 'cult-cat'){
+
+  var randName = [];
+  do {
+    randName[randName.length] = cult.splice(
+                                  Math.floor(Math.random() * cult.length)
+                                , 1)[0];
+  } while (randName.length < 3);
+$('#one').html(randName[0]);
+$('select').hide();
+$('.selected').hide();
+  console.log(randName);
+}
+// Hvis activity kategori er valgt
+if (selectedCat == 'act-cat'){
+
+  var randName = [];
+  do {
+    randName[randName.length] = activity.splice(
+                                  Math.floor(Math.random() * activity.length)
+                                , 1)[0];
+  } while (randName.length < 3);
+$('#one').html(randName[0]);
+$('select').hide();
+$('.selected').hide();
+  console.log(randName);
+}
 });
 
 
